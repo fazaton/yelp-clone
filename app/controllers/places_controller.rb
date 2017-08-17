e1d488f61a6468ab06004a1229a7047d33daece8
fazaton <fazaton@gmail.com>
2017-08-17 00:59:13 +0000

    input form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -6,4 +6,14 @@ class PlacesController < ApplicationController
     def new
         @place = Place.new
     end
+    
+    def create
+        Place.create(place_params)
+    end
+    
+    private
+    
+    def place_params
+        params.require(:place).permit(:name, :address, :description)
+    end
 end
