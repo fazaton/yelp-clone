ee86175a20fcaca98ea0a8ce708cf3fd87b0f245
fazaton <fazaton@gmail.com>
2017-08-18 21:33:18 +0000

    user validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-    before_action :authenticate_user!, only: [:new, :create]
+    before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
     
     def index
         @places = Place.order(:name).page params[:page]
@@ -10,10 +10,48 @@ class PlacesController < ApplicationController
     end
     
     def create
-        current_user.places.create(place_params)
-        redirect_to root_path
+        @place = current_user.places.create(place_params)
+        if @place.valid?
+            redirect_to root_path
+        else
+            render :new, status: :unprocessable_entity
+        end
     end
     
+    def show
+       @place = Place.find(params[:id]) 
+    end
+    
+    def edit 
+       @place = Place.find(params[:id]) 
+       
+        if @place.user != current_user
+           return render text: 'Not allowed', status: :forbidden
+        end
+    end
+    
+    def update
+       @place = Place.find(params[:id])
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
+       @place.update_attributes(place_params)
+       if @place.valid?
+            redirect_to root_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+    
+    def destroy 
+       @place = Place.find(params[:id]) 
+        if @place.user != current_user
+            return render text: 'Not Allowed', status: :forbidden
+        end
+       @place.destroy
+       redirect_to root_path
+    end
+        
     private
     
     def place_params
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ApplicationRecord
     paginates_per 5
     belongs_to :user
+    validates :name, presence: true, length: { minimum: 3 }
+    validates :description, presence: true
+    validates :address, presence: true
 end
